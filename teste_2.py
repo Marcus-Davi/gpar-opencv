acd53d0afea00d84466e192d04a4a1e1d4dd3295
Vinicius <viniciuseletro@gmail.com>
2020-02-20 02:17:46 -0300

    detecta bola estatica com filtros

--- teste_2.py
+++ teste_2.py
@@ -14,28 +14,65 @@ height = int(img.shape[0] * scale_percent / 100)
 dim = (width, height)
 img = cv2.resize(img,dim)
 
+# Borra imagem para reduzir ruidos
+#borra_img = cv2.GaussianBlur(img,(5,5),0)
+
 #Converte imagem de RGB para HSV
 img_hsv=cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
 #Converte imagem de RGB para P&B
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-# Teste de cor imagem
-for i in range(0,255,1):
-    lower_blue = np.array([i,100,100])
-    upper_blue = np.array([i+20,255,255])
-    print i
 
-    # Threshold the HSV image to get only blue colors
-    mask = cv2.inRange(img_hsv, lower_blue, upper_blue)
-    cv2.imshow('teste',mask)
-    cv2.waitKey(220)
-'''
-for i in range(0,200,10):
-    ret, thresh = cv2.threshold(gray,30,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-    #cv2.imshow("Imagem inicial",img)
-    cv2.imshow("Imagem Gray",thresh)
-    print(i)
-    cv2.waitKey(200)
-'''
+# Detecta cor vermelha
+lower_blue = np.array([0,100,100])
+upper_blue = np.array([8,255,255])
+
+# Threshold the HSV image to get only blue colors
+pb = cv2.inRange(img_hsv, lower_blue, upper_blue)
+cv2.imshow('Imagem P&B',pb)
+cv2.waitKey(0)
+
+# Filtragem de imagem - Reduz ruidos
+nf = 2
+kernel = np.ones((nf,nf),np.uint8)
+erosion = cv2.erode(pb,kernel,iterations = 3)
+cv2.imshow('erosion',erosion)
+cv2.waitKey(0)
+
+# Filtragem de imagem - Preenche vazios
+dilation = cv2.dilate(erosion,kernel,iterations = 1)
+cv2.imshow('dilation',dilation)
+cv2.waitKey(0)
+
+# Filtragem de imagem - Erosion seguido de dilation
+opening = cv2.morphologyEx(pb, cv2.MORPH_OPEN, kernel)
+cv2.imshow('opening',opening)
+cv2.waitKey(0)
+
+# Adiciona filtro, borrando imagem
+img_ftd = cv2.GaussianBlur(opening,(5,5),0)
+
+detected_circles = cv2.HoughCircles(img_ftd, 
+				cv2.HOUGH_GRADIENT, 1, 20, param1 = 50, 
+			param2 = 30, minRadius = 1, maxRadius = 80) 
+
+# Draw circles that are detected. 
+if detected_circles is not None: 
+
+	# Convert the circle parameters a, b and r to integers. 
+	detected_circles = np.uint16(np.around(detected_circles)) 
+	print(detected_circles)
+
+	for pt in detected_circles[0, :]: 
+		a, b, r = pt[0], pt[1], pt[2] 
+
+		# Draw the circumference of the circle. 
+		cv2.circle(img, (a, b), r, (0, 255, 0), 2) 
+
+		# Draw a small circle (of radius 1) to show the center. 
+		cv2.circle(img, (a, b), 1, (0, 0, 255), 3) 
+		cv2.imshow("Detected Circle", img) 
+		cv2.waitKey(0)            
+
 
 cv2.destroyAllWindows()
