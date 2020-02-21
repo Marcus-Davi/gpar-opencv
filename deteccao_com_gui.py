e780e5b039b807fe1b06a77706fb1ac0f09ef57a
Vinicius <viniciuseletro@gmail.com>
2020-02-21 17:37:42 -0300

    deteccao com guide ver2

--- deteccao_com_gui.py
+++ deteccao_com_gui.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import time
 
+
 def nothing(x):
     pass
 
@@ -12,6 +13,9 @@ def nothing(x):
 # Create a black image, a window
 txt_inf = 'CALIBRA_INFERIOR'
 txt_sup = 'CALIBRA_SUPERIOR'
+txt_ft = 'Filtro'
+
+#############################################
 
 # CALIBRA INFERIOR
 paletainf = np.zeros((200,200,3), np.uint8)
@@ -19,8 +23,10 @@ cv2.namedWindow(txt_inf)
 
 # create trackbars for color change
 cv2.createTrackbar('H',txt_inf,0,180,nothing)
-cv2.createTrackbar('S',txt_inf,0,255,nothing)
-cv2.createTrackbar('V',txt_inf,0,255,nothing)
+cv2.createTrackbar('S',txt_inf,50,255,nothing)
+cv2.createTrackbar('V',txt_inf,50,255,nothing)
+
+#############################################
 
 # CALIBRA SUPERIOR
 paletasup = np.zeros((200,200,3), np.uint8)
@@ -28,9 +34,31 @@ cv2.namedWindow(txt_sup)
 
 # create trackbars for color change
 cv2.createTrackbar('H',txt_sup,0,180,nothing)
-cv2.createTrackbar('S',txt_sup,0,255,nothing)
-cv2.createTrackbar('V',txt_sup,0,255,nothing)
+cv2.createTrackbar('S',txt_sup,255,255,nothing)
+cv2.createTrackbar('V',txt_sup,255,255,nothing)
 
+#############################################
+
+# CAIXA FILTROS
+
+cx_filtro = np.zeros((1,300,3), np.uint8)
+cv2.namedWindow(txt_ft)
+cv2.createTrackbar('Denoise',txt_ft,0,20,nothing)
+#cv2.createTrackbar('Denoise_qtde',txt_ft,0,20,nothing)
+cv2.createTrackbar('Erosao',txt_ft,0,10,nothing)
+#cv2.createTrackbar('Erosao_qtde',txt_ft,0,10,nothing)
+cv2.createTrackbar('Dilatacao',txt_ft,0,10,nothing)
+#cv2.createTrackbar('Dilatacao_qtde',txt_ft,0,10,nothing)
+cv2.createTrackbar('Erosao_Dilatacao',txt_ft,0,10,nothing)
+#cv2.createTrackbar('Erosao_Dilatacao_qtde',txt_ft,0,10,nothing)
+cv2.createTrackbar('Blur',txt_ft,0,10,nothing)
+#cv2.createTrackbar('Blur_qtde',txt_ft,0,10,nothing)
+cv2.createTrackbar('Par1',txt_ft,0,100,nothing)
+cv2.createTrackbar('Raio_max',txt_ft,50,100,nothing)
+cv2.createTrackbar('Raio_min',txt_ft,0,100,nothing)
+cv2.createTrackbar('Raio_max',txt_ft,50,100,nothing)
+
+###################################
 
 # Read image.
 #img = cv2.imread('bola.jpg', cv2.IMREAD_COLOR)
@@ -38,6 +66,29 @@ cap = cv2.VideoCapture(0)
 
 
 while(1):
+
+#####################################################################
+############ CAPTURA DE IMAGEM ######################################
+#####################################################################
+
+    # Captura imagem
+    _, img_orig = cap.read()
+
+    # Inverte imagem para deixar "correta" a exibicao
+    img_orig = cv2.flip(img_orig,1)
+
+    # Redimensiona imagem
+    scale_percent = 60  # percent of original size
+    width = int(img_orig.shape[1] * scale_percent / 100)
+    height = int(img_orig.shape[0] * scale_percent / 100)
+    dim = (width, height)
+    img_orig = cv2.resize(img_orig, dim)
+
+#####################################################################
+########### FILTROS GUIDE ###########################################
+#####################################################################
+
+    #######################################
 
     #### CALIBRACAO INFERIOR #####
     cv2.imshow(txt_inf,paletainf)
@@ -48,6 +99,8 @@ while(1):
     pinf = paletainf[1][1][:]
     paletainf = cv2.cvtColor(paletainf, cv2.COLOR_HSV2BGR)
 
+    #######################################
+
     #### CALIBRACAO SUPERIOR #####
     cv2.imshow(txt_sup,paletasup)
     hsup = cv2.getTrackbarPos('H',txt_sup)
@@ -56,59 +109,99 @@ while(1):
     paletasup[:] = [hsup,ssup,vsup]
     psup = paletasup[1][1][:]
     paletasup = cv2.cvtColor(paletasup, cv2.COLOR_HSV2BGR)
+
+    #######################################
+
+    #### CAIXA DE FILTROS #####
+    # Pegando valores da caixa
+    cv2.imshow(txt_ft,cx_filtro)
+    erose_sw = cv2.getTrackbarPos('Erosao',txt_ft)
+    #erose_qt = cv2.getTrackbarPos('Erosao_qtde',txt_ft)
+    dilation_sw = cv2.getTrackbarPos('Dilatacao',txt_ft)
+    #dilation_qt = cv2.getTrackbarPos('Dilatacao_qtde',txt_ft)
+    eros_dil_sw = cv2.getTrackbarPos('Erosao_Dilatacao',txt_ft)
+    denoise_sw = cv2.getTrackbarPos('Denoise',txt_ft)
+    #denoise_qt = cv2.getTrackbarPos('Denoise_qtde',txt_ft)
+    blur_sw = cv2.getTrackbarPos('Blur',txt_ft)
+    #blur_qt = cv2.getTrackbarPos('Blur_qtde',txt_ft)
    
-    # Take each frame
-    _, img = cap.read()
+#####################################################################
+####### IMPLEMENTA FILTROS DA GUIDE #################################
+##################################################################### 
+    result = img_orig
+    #######################################
+    
+    # Removendo ruidos
+    if denoise_sw == 0:
+        result = img_orig
+        cv2.destroyWindow('Denoising')
+    else:
+        img_denoised = cv2.fastNlMeansDenoisingColored(img_orig,None,10,10,7,denoise_sw)      
+        result = img_denoised  
+        cv2.imshow('Denoising',img_denoised)
+    
+    #######################################
 
-    # Inverte imagem para deixar "correta" a exibicao
-    img = cv2.flip(img,1)
-
-    # Redimensiona imagem
-    scale_percent = 60  # percent of original size
-    width = int(img.shape[1] * scale_percent / 100)
-    height = int(img.shape[0] * scale_percent / 100)
-    dim = (width, height)
-    img = cv2.resize(img, dim)
-     
     # Converte imagem de RGB para HSV
-    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
+    img_hsv = cv2.cvtColor(result, cv2.COLOR_BGR2HSV)
+    
     # Define cores detectaveis
     lower_blue = np.array([pinf])
     upper_blue = np.array([psup])
-    #print pinf1
-    #print pinf_hsv_1r1c
     
     # Threshold the HSV image to get only blue colors
-    pb = cv2.inRange(img_hsv, lower_blue, upper_blue)
-    #cv2.imshow('Imagem P&B', pb)
-    # cv2.waitKey(0)
-
-    # Filtragem de imagem - Reduz ruidos    
-    nf = 1
-    kernel_er = np.ones((nf, nf), np.uint8)
-    erosion = cv2.erode(pb, kernel_er, iterations=3)
-    cv2.imshow('erosion', erosion)
-    # cv2.waitKey(0)
+    result = cv2.inRange(img_hsv, lower_blue, upper_blue)
+    
+    #######################################
+    
+    # Filtragem de imagem - Reduz ruidos  
+    if erose_sw == 0:
+        result = result
+        cv2.destroyWindow('erosion')
+    else:
+        nf = erose_sw
+        kernel_er = np.ones((nf, nf), np.uint8)
+        result = cv2.erode(result, kernel_er, iterations=3)
+        erosed = result
+        cv2.imshow('erosion', erosed)
+        
+    #######################################
 
     # Filtragem de imagem - Preenche vazios
-    nf = 2
-    kernel_dil = np.ones((nf, nf), np.uint8)
-    dilation = cv2.dilate(erosion, kernel_dil, iterations=1)
-    cv2.imshow('dilation', dilation)
-    # cv2.waitKey(0)
+    if dilation_sw == 0:
+        cv2.destroyWindow('dilation')
+    else:
+        nf = dilation_sw
+        kernel_dil = np.ones((nf, nf), np.uint8)
+        result = cv2.dilate(result, kernel_dil, iterations=1)
+        dilation = result
+        cv2.imshow('dilation', dilation)
+
+    #######################################
 
     # Filtragem de imagem - Erosion seguido de dilation
-    nf = 2
-    kernel_opn = np.ones((nf, nf), np.uint8)
-    opening = cv2.morphologyEx(pb, cv2.MORPH_OPEN, kernel_opn)
-    #cv2.imshow('opening', opening)
-    # cv2.waitKey(0)
 
+    if eros_dil_sw == 0:
+        cv2.destroyWindow('Erosion_plus_Dilation')
+    elif erose_sw == 0 and dilation_sw == 0:
+        nf = eros_dil_sw
+        kernel_opn = np.ones((nf, nf), np.uint8)
+        result = cv2.morphologyEx(result, cv2.MORPH_OPEN, kernel_opn)
+        opening = result
+        cv2.imshow('Erosion_plus_Dilation', opening)
+
+    #######################################
+    
+    # BORRA A IMAGEM
     # Adiciona filtro, borrando imagem
-    img_ftd = cv2.GaussianBlur(opening, (5, 5), 0)
-
-    detected_circles = cv2.HoughCircles(img_ftd,
+    if blur_sw == 0:
+        cv2.destroyWindow('Img_Borrada')
+    else:
+        result = cv2.GaussianBlur(result, (5, 5), 0)
+        opening = result
+        cv2.imshow('Img_Borrada', opening)
+    
+    detected_circles = cv2.HoughCircles(result,
                                         cv2.HOUGH_GRADIENT, 1, 20, param1=50,
                                         param2=30, minRadius=1, maxRadius=80)
 
@@ -123,14 +216,14 @@ while(1):
             a, b, r = pt[0], pt[1], pt[2]
 
             # Draw the circumference of the circle.
-            cv2.circle(img, (a, b), r, (0, 255, 0), 2)
+            cv2.circle(img_orig, (a, b), r, (0, 255, 0), 2)
 
             # Draw a small circle (of radius 1) to show the center.
-            cv2.circle(img, (a, b), 1, (0, 0, 255), 3)
+            cv2.circle(img_orig, (a, b), 1, (0, 0, 255), 3)
         
-    cv2.imshow("Imagem filtrada", opening)
-    cv2.imshow("Deteccao", img)
-    cv2.imshow("Imagem sem filtro", pb)
+    
+    cv2.imshow("Imagem original", img_orig)
+    cv2.imshow("Imagem com filtros", result)
 
     k = cv2.waitKey(1) & 0xFF
     if k == 27:
